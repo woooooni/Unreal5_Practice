7da182a3a105fe9696a2f880745c442a2a6a9fa2
woooooni <rlaxodnjs6574@gmail.com>
2022-05-15 04:58:30 +0900

    22.05.15 Start

---
+++ Source/UE5_Practice/UE5_Practice.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE5_Practice.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5_Practice, "UE5_Practice" );
---
+++ Source/UE5_Practice/UE5_PracticeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5_PracticeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_PRACTICE_API AUE5_PracticeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
